1cd32167689d9a0723b0ddee4ccc20a52ff7e708
KneeCapStealer <87864377+KneeCapStealer@users.noreply.github.com>
2023-11-16 14:32:38 +0100

    Added a showcase on how to use buttons in godot

--- scripts/StateMachine.gd
+++ scripts/StateMachine.gd
@@ -1,16 +1,19 @@
 extends Node
 
+# Timer and day counter
 var _dayTimer: Timer
+var _daySpeed: float = 1
 var _dayCount: int = 0
 
 signal OnNextDay
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+    # Start the day timer
     _dayTimer = Timer.new()
-    _dayTimer.autostart = true
     _dayTimer.wait_time = 1
     add_child(_dayTimer)
+    _dayTimer.start()
     _dayTimer.timeout.connect(_IncementDayCounter)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -22,3 +25,12 @@ func _IncementDayCounter():
     _dayCount += 1
     print(_dayCount)
     OnNextDay.emit()
+
+
+func ChangeDaySpeed(speed: float):
+    # This updates the timers speed immediatly
+    var timeRatio = _dayTimer.wait_time / (1 / speed)
+    _dayTimer.wait_time = _dayTimer.time_left / timeRatio  # This scales the remaining time by ratio
+    _dayTimer.stop()
+    _dayTimer.start()  # restart the timer with the new remaining time
+    _dayTimer.wait_time = 1 / speed  # set the timer to it's actual speed
