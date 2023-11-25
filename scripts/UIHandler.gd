d7c1c52ac7352f67259f01f4bd9517c15e5318bf
amandaseier <aman4884@edu.aarhustech.dk>
2023-11-25 10:16:20 +0100

    Pause menu & start menu added !!!

--- scripts/UIHandler.gd
+++ scripts/UIHandler.gd
@@ -5,3 +5,6 @@ func ChangeDaySpeed(speed: float):
 
 func StopDayCycle():
 	StateMachine.StopDayCycle()
+
+func _on_pause_menu_button_pressed():
+	get_tree().change_scene_to_file("res://pause_menu.tscn")
