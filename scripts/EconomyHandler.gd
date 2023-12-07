45a1b5581204ea3b5d1744b4cee99ae31b0a8438
J0pper <Jeppe.vadandersen@gmail.com>
2023-12-07 20:50:42 +0100

    Basic economy system

---
+++ scripts/EconomyHandler.gd
@@ -0,0 +1,38 @@
+extends Node 
+
+
+var cancerPoints: int = 0
+
+# arbitrary placeholder variables
+# ------------
+var sizeDifference: int = 100
+# in case of money per payout scaler 
+var moneyScale: float = 1.
+# ------------
+
+var payoutDelay: int = 10 # 10 days
+var daysTillPayout: int = payoutDelay
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	StateMachine.OnNextDay.connect(_CalcCancerPoints)
+
+func _stuff():
+	print("Amount of cancer colonies: ", StateMachine.colonies)
+
+
+func _CalcCancerPoints():
+	if not daysTillPayout == 0:
+		daysTillPayout -= 1
+		return
+	daysTillPayout = payoutDelay
+	
+	cancerPoints += sizeDifference * moneyScale
+	self.text = "Cancer points: {CP}".format({"CP": cancerPoints})
+	
+func _AddCancerPoints():
+	pass
+
+func _RemoveCancerPoints():
+	pass
--- scripts/cells/Colony.gd
+++ scripts/cells/Colony.gd
@@ -9,35 +9,36 @@ var _growChance: float = 6.9
 var _rng = RandomNumberGenerator.new()
 
 func _ready():
-    sprite = get_node("Sprite2D")
-    StateMachine.OnNextDay.connect(_TrySpread)
-    StateMachine.colonies.append(self)
+	sprite = get_node("Sprite2D")
+	StateMachine.OnNextDay.connect(_TrySpread)
+	StateMachine.OnNextDay.connect(_TrySpread)
+	StateMachine.colonies.append(self)
 
-    if active:
-        ActivateColony()
+	if active:
+		ActivateColony()
 
 func ActivateColony():
-    active = true
-    sprite.visible = true
+	active = true
+	sprite.visible = true
 
 func KillColony():
-    active = false
-    sprite.visible = false
+	active = false
+	sprite.visible = false
 
 func _TrySpread():
-    if not active:
-        return
+	if not active:
+		return
 
 
-    for colony in StateMachine.colonies:
-        if colony == self:
-            continue
+	for colony in StateMachine.colonies:
+		if colony == self:
+			continue
 
-        if self.position.distance_to(colony.position) <= StateMachine.upgrades.get("range"):
-            colony.TryGrow()
+		if self.position.distance_to(colony.position) <= StateMachine.upgrades.get("range"):
+			colony.TryGrow()
 
 # When being spread to, try and grow
 func TryGrow():
-    if _rng.randf_range(0.0, 100.0) < _growChance:
-        ActivateColony()
+	if _rng.randf_range(0.0, 100.0) < _growChance:
+		ActivateColony()
 
