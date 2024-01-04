76a78fd37643d4d5c4b06c4ce4db1f756667bb0a
KasperThorup25 <77360109+KasperThorup25@users.noreply.github.com>
2024-01-04 23:40:17 +0100

    added updated healthbar

--- scripts/UIHandler.gd
+++ scripts/UIHandler.gd
@@ -2,39 +2,39 @@ extends Node
 
 
 func _ready():
-	print("HELLO")
-	StateMachine.ResumeDayCycle()
-	
-	# Upgrade tree scene
-	get_node("UpgradeMenu/Background/backButton").connect("pressed", backButtonPressed)
-	get_node("PauseMenu/CenterContainer/VBoxContainer/Resume button").connect("pressed", backButtonPressed)
-	
-	# Settings menu scene
-	# get_node("PauseMenu/CenterContainer/VBoxContainer/Resume button").connect("pressed", backButtonPressed)
-	
+    print("HELLO")
+    StateMachine.ResumeDayCycle()
+    
+    # Upgrade tree scene
+    get_node("UpgradeMenu/Background/backButton").connect("pressed", backButtonPressed)
+    get_node("PauseMenu/CenterContainer/VBoxContainer/Resume button").connect("pressed", backButtonPressed)
+    
+    # Settings menu scene
+    # get_node("PauseMenu/CenterContainer/VBoxContainer/Resume button").connect("pressed", backButtonPressed)
+    
 
 func ChangeDaySpeed(speed: float):
-	StateMachine.ChangeDaySpeed(speed)
+    StateMachine.ChangeDaySpeed(speed)
 
 
 func StopDayCycle():
-	StateMachine.StopDayCycle()
+    StateMachine.StopDayCycle()
 
 
 func _on_pause_menu_button_pressed():
-	get_node("PauseMenu").visible = true
-	
-	StateMachine.StopDayCycle()
+    get_node("PauseMenu").visible = true
+    
+    StateMachine.StopDayCycle()
 
 
 func _on_upgrade_pressed():
-	get_node("UpgradeMenu").visible = true
-	
-	StateMachine.StopDayCycle()
+    get_node("UpgradeMenu").visible = true
+    
+    StateMachine.StopDayCycle()
 
 
 func backButtonPressed():
-	get_node("PauseMenu").visible = false
-	get_node("UpgradeMenu").visible = false
-	
-	StateMachine.ResumeDayCycle()
+    get_node("PauseMenu").visible = false
+    get_node("UpgradeMenu").visible = false
+    
+    StateMachine.ResumeDayCycle()
--- scripts/bg_music.gd
+++ scripts/bg_music.gd
@@ -3,17 +3,17 @@ extends AudioStreamPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# Start playing when the game starts
-	# this scene (BackgroundMusic) is autoloaded at the start of the game
-	# play()
-	print("Playing music")
-	
-	# the music need to toogle depending on a signal
-	# connect "toggleMusic" to this signal xxx
+    # Start playing when the game starts
+    # this scene (BackgroundMusic) is autoloaded at the start of the game
+    # play()
+    print("Playing music")
+    
+    # the music need to toogle depending on a signal
+    # connect "toggleMusic" to this signal xxx
 
 
 func toggleMusic():
-	if playing:
-		stop()
-	else:
-		play() 
+    if playing:
+        stop()
+    else:
+        play() 
