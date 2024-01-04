94fa4c42c529eba86577bd1197d78c209571ff3b
KasperThorup25 <77360109+KasperThorup25@users.noreply.github.com>
2024-01-04 22:47:41 +0100

    kemo behandling
    Patienten kan nu modtage kemobehandling på tilfældig basis med regelmæssige intervaller. Variabler til kemoen kan ændres i inspector panellet

--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -5,16 +5,16 @@ extends Control
 
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/Main_Scene.tscn")
+    get_tree().change_scene_to_file("res://scenes/Main_Scene.tscn")
 
 
 func _on_quit_button_pressed():
-	get_tree().quit()
+    get_tree().quit()
 
 
 func _on_settings_button_pressed():
-	get_node("SettingsMenu").visible = true
+    get_node("SettingsMenu").visible = true
 
 
 func backButtonPressed():
-	get_node("SettingsMenu").visible = false
+    get_node("SettingsMenu").visible = false
