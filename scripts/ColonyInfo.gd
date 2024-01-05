5987211cb3ef732f5573a832c340adfd33678b57
J0pper <Jeppe.vadandersen@gmail.com>
2024-01-05 12:14:07 +0100

    Colonyinfo works again YUBIIIIIIIIII

--- scripts/ColonyInfo.gd
+++ scripts/ColonyInfo.gd
@@ -6,8 +6,13 @@ extends ColorRect
 
 @onready var colonyGroup := get_node("../Colonies")
 
+var highlightedColony: String = ""
 
-# var displayColonyAttributes = [Size, GrowthSpeed, OrganDamage]
+var colonyInfoText: Dictionary = {
+    "Size": 0,
+    "GrowthSpeed": 0,
+    "OrganDamage": 0
+    }
 
 
 # Called when the node enters the scene tree for the first time.
@@ -19,20 +24,21 @@ func _ready():
 
 # Updates the positioning and textual contents of the colony info box.
 func _UpdateLabel(colony):
-    if colonyInfo.visible:
-        colonyInfo.hide()
+    if colony.get_name() == highlightedColony:
+        colonyInfo.visible = false
+        highlightedColony = ""
         return
-    else:
-        colonyInfo.show()
+    colonyInfo.visible = true
+    highlightedColony = colony.get_name()
     
     colonyInfo.global_position = _CalcOffset(colony)
     
-    
+
     var labelStr: String = ""
-    for key in StateMachine.colonies[colony.get_name()].keys():
+    for key in colonyInfoText.keys():
         print(key)
         var keyStr: String = "{key}: {information}\n"
-        labelStr += keyStr.format({"key": str(key), "information": str(StateMachine.colonies[colony.get_name()][key])})
+        labelStr += keyStr.format({"key": str(key), "information": str(colonyInfoText[key])})
     
     colonyName.text = str(colony.get_name())
     colonyInformation.text = str(labelStr)
@@ -40,12 +46,12 @@ func _UpdateLabel(colony):
 
 # Calculates the positioning of the colonyinfo box.
 func _CalcOffset(colony) -> Vector2:
-    var constOffset 				= Vector2(50, 25)
-    var variableOffset: Vector2 	= colonyInfo.size / 2
+    var constOffset             = Vector2(50, 25)
+    var variableOffset: Vector2 = colonyInfo.size / 2
     var totalOffset: Vector2
-    var colonyPos: Vector2 			= colony.global_position
-    var colonySize: Vector2 		= colony.size
-    var viewPortSize: Vector2 		= get_viewport_rect().size
+    var colonyPos: Vector2      = colony.global_position
+    var colonySize: Vector2     = colony.size
+    var viewPortSize: Vector2   = get_viewport_rect().size
     
     if colonyPos[0] > viewPortSize[0] / 2:
         constOffset[0] = -constOffset[0]
--- scripts/Healthbar.gd
+++ scripts/Healthbar.gd
@@ -1,7 +1,6 @@
 extends Sprite2D
 
 var _health: float = 100.
-var _dailyDamageTaken: float
 var _dailyHealthGained: float
 
 var frameIndex: int = 0
@@ -12,6 +11,9 @@ var _kemoActive: bool = false
 @export var _kemoHealth: float = 5
 @export var _kemoTotalEvents: int = 10
 var _kemoFinishedEvents: int = 0
+
+const damagePrDay: float = 0.2
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -34,23 +36,28 @@ func _randomPatientMoves():
 
 func _calcHealthGained():
     _dailyHealthGained = 0
-    if _kemoActive:
-        if _kemoFinishedEvents < _kemoTotalEvents:
-            if !(StateMachine._dayCount % _kemoDageImellemEvents):
-                _dailyHealthGained = _kemoHealth
-                print("Patienten har nu været til kemo behandling")
-                _kemoFinishedEvents += 1
-        else:
-            _kemoFinishedEvents = 0
-            _kemoActive = false
+    if not _kemoActive:
+        return
+        
+    if not _kemoFinishedEvents < _kemoTotalEvents:
+        _kemoFinishedEvents = 0
+        _kemoActive = false
+        return
+    
+    if StateMachine._dayCount % _kemoDageImellemEvents != 0:
+        return
+        
+    _dailyHealthGained = _kemoHealth
+    print("Patienten har nu været til kemo behandling")
+    _kemoFinishedEvents += 1
 
 
 func _calcHealth():
-    _dailyDamageTaken = StateMachine.upgradeHandler.GetUpgradeValues()["damage_multiplyer"]
+    var dailyDamageTaken: float = damagePrDay * StateMachine.upgradeHandler.GetUpgradeValues()["damage_multiplyer"]
     
     if _health > 0:
         # Update the health variable 
-        _health -= _dailyDamageTaken
+        _health -= dailyDamageTaken
         _health += _dailyHealthGained
         print("Health: ", _health)
         scale.x = _health * 0.1 # scale.x = 10 is the same as 100% health
