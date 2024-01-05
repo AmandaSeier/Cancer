2c00a23d5174db9ccf97b1dda92a8794f04716ee
J0pper <Jeppe.vadandersen@gmail.com>
2024-01-05 15:10:24 +0100

    Reverted main scene after some merge problems

--- scripts/settings_menu.gd
+++ scripts/settings_menu.gd
@@ -2,12 +2,12 @@ extends Control
 
 
 func _on_back_pressed():
-	self.visible = false
+    self.visible = false
 
 
 func _on_music_toggled(button_pressed):
-	BgMusic.toggleMusic()
+    BgMusic.toggleMusic()
 
 
 func _on_sound_toggled(button_pressed):
-	print("i am not currently doing anything, Find me and make me do stuff :P")
+    print("i am not currently doing anything, Find me and make me do stuff :P")
