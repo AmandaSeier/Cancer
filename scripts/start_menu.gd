66274a852677706ce3a46987c58b6d2ec8ed55d6
amandaseier <aman4884@edu.aarhustech.dk>
2024-01-09 19:46:37 +0100

    pushing so that i can pull bruh

--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -1,20 +1,20 @@
 extends Control
 
 func _ready():
-    self.visible = true
-    
+	self.visible = true
+	
 
 func _on_start_button_pressed():
-    self.visible = false
+	self.visible = false
 
 
 func _on_quit_button_pressed():
-    get_tree().quit()
+	get_tree().quit()
 
 
 func _on_settings_button_pressed():
-    get_node("../SettingsMenu").visible = true  
+	get_node("../SettingsMenu").visible = true  
 
 
 func _on_credits_button_pressed():
-    print("I am cool credits button and i dont do anything. Put some credits in me.")
+	print("I am cool credits button and i dont do anything. Put some credits in me.")
