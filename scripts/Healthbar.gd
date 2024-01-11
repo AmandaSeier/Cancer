a040a36374d02307dcd6756fd1d458cdf9154691
KneeCapStealer <87864377+KneeCapStealer@users.noreply.github.com>
2024-01-11 13:25:09 +0100

    Made the patient take dmg based on cell count

--- scripts/Healthbar.gd
+++ scripts/Healthbar.gd
@@ -3,111 +3,121 @@ extends Sprite2D
 var _health := 100.
 
 var frameIndex := 0
-
+@export_category("Discorver cancer")
 @export var _discoverCancerCellNum := 2_000_000
 
-# Kemo
+@export_category("Cancer damage")
+@export_range(0, 1_000_000) var _minCellDmgNum := 0
+@export_range(1_000_000, 7_500_000) var _maxCellDmgNum := 1_000_000
+@export var _minDmg := 0.04
+@export var _maxDmg := 1.
+var _dmgPrDay := _minDmg
+
+@export_category("cemo therapy")
 @export var _cemoChance := 2. # in percent
 @export var _cemoCooldownDays := 7 # 1 gang om ugen
 @export var _cemoCancerDamage := 500_000
-@export var _cemoCancerDmg := 250_000
+@export var _cemoCancerDmgVariation := 250_000
 @export var _cemoDamage := 2.
 @export var _cemoTotalEvents := 10
 var _cemoActive := false
 var _cemoFinishedEvents := 0
 
-# Operation
+@export_category("Tumor removal operations")
 @export var _operationCellNum := 3_000_000
 @export var _operationCooldownDays := 60
 var _operationDays := 0
 
-const damagePrDay := 0.2
 var rng := RandomNumberGenerator.new()
 var cancerFound := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    StateMachine.OnNextDay.connect(_HealthCalculations)	
+	StateMachine.OnNextDay.connect(_HealthCalculations)	
 
 
 func _HealthCalculations():
-    _ActivateHealthEvents()
-    _GetTreatment()
-    _UpdateHealth()
+	_ActivateHealthEvents()
+	_GetTreatment()
+	_UpdateHealth()
 
 
-func _ActivateHealthEvents():
-    if not cancerFound:
-        var num := rng.randf_range(0, 100)
-        if num <= StateMachine.GetTotalNumCells() / (_discoverCancerCellNum as float):
-            cancerFound = true
-            print("HE KNOWS PANIIIC!!")
-        return
+func _ActivateHealthEvents() -> void:
+	if not cancerFound:
+		var num := rng.randf_range(0, 100)
+		if num <= StateMachine.GetTotalNumCells() / (_discoverCancerCellNum as float):
+			cancerFound = true
+			print("HE KNOWS PANIIIC!!")
+		return
 
-    var num := rng.randf_range(0, 100)
-    # random events
-    if num <= _cemoChance:
-        _cemoActive = true
-        print("He will now get cemo")
+	var num := rng.randf_range(0, 100)
+	# random events
+	if num <= _cemoChance:
+		_cemoActive = true
+		print("He will now get cemo")
 
 
 func _GetCemo() -> void:
-    if not _cemoActive:
-        return
-        
-    if not _cemoFinishedEvents < _cemoTotalEvents:
-        _cemoFinishedEvents = 0
-        _cemoActive = false
-        return
-    
-    if StateMachine.GetCurrentDay() % _cemoCooldownDays != 0:
-        return
-        
-    for colony in StateMachine.colonies:
-        colony.DamageColony(_cemoCancerDamage + randi_range(-_cemoCancerDmg, _cemoCancerDmg))
+	if not _cemoActive:
+		return
+		
+	if not _cemoFinishedEvents < _cemoTotalEvents:
+		_cemoFinishedEvents = 0
+		_cemoActive = false
+		return
+	
+	if StateMachine.GetCurrentDay() % _cemoCooldownDays != 0:
+		return
+		
+	for colony in StateMachine.colonies:
+		colony.DamageColony(_cemoCancerDamage + randi_range(-_cemoCancerDmgVariation, _cemoCancerDmgVariation))
 
-    _health -= _cemoDamage
+	_health -= _cemoDamage
 
-    _cemoFinishedEvents += 1
-    print("Finished cemo event number ", _cemoFinishedEvents, " out of ", _cemoTotalEvents)
+	_cemoFinishedEvents += 1
+	print("Finished cemo event number ", _cemoFinishedEvents, " out of ", _cemoTotalEvents)
 
 
 func _GetTreatment() -> void:
-    if not cancerFound:
-        return
+	if not cancerFound:
+		return
 
-    _GetCemo()
+	_GetCemo()
 
-    # operation
-    if StateMachine.GetTotalNumCells() > _operationCellNum:
-        if _operationDays >= _operationCooldownDays:
-            StateMachine.GetActiveColonies().pick_random().KillColony()
-            _operationDays = 0
-        else:
-            _operationDays += 1
+	# operation
+	if StateMachine.GetTotalNumCells() > _operationCellNum:
+		if _operationDays >= _operationCooldownDays:
+			StateMachine.GetActiveColonies().pick_random().KillColony()
+			_operationDays = 0
+		else:
+			_operationDays += 1
 
 
-func _UpdateHealth():
-    var dailyDamageTaken: float = damagePrDay * StateMachine.upgradeHandler.GetUpgradeValues()["damage_multiplyer"]
-    
-    if _health > 0:
-        # Update the health variable 
-        _health -= dailyDamageTaken
-        _health += _dailyHealthGained
-        print("Health: ", _health)
-        scale.x = _health * 0.1 # scale.x = 10 is the same as 100% health
-        if _health >= 100:
-            print("GAME OVER")
-            # call game over scene
-    else:
-        _health = 0
-        print("Yaiii you won")
-        # call game won scene
+func _UpdateHealth() -> void:
+	if StateMachine.GetTotalNumCells() < _minCellDmgNum:
+		_dmgPrDay = _minDmg
+	elif StateMachine.GetTotalNumCells() > _maxCellDmgNum:
+		_dmgPrDay = _maxDmg
+	else:
+		var t := (StateMachine.GetTotalNumCells() - _minCellDmgNum) / (_maxCellDmgNum - _minCellDmgNum)
+		_dmgPrDay = lerpf(_minDmg, _maxDmg, t)
+	print(_dmgPrDay)
+	var dailyDamageTaken: float = _dmgPrDay * StateMachine.upgradeHandler.GetUpgradeValues()["damage_multiplyer"]
+	print(dailyDamageTaken)
+	if _health > 0:
+		# Update the health variable 
+		_health -= dailyDamageTaken
+		print("Health: ", _health)
+		scale.x = _health * 0.1 # scale.x = 10 is the same as 100% health
+	else:
+		_health = 0
+		print("Yaiii you won")
+		# call game won scene
 
-    #######################################################################
-    # PLEASE FJERN DET HER SENERE DET ER UDELUKKENDE FOR TEST AF BAGGRUNDE
-    if StateMachine._dayCount / 20.0 == frameIndex+1:
-        frameIndex += 1
-        get_node("../../Background_Stue").frame = frameIndex
-    #######################################################################
+	#######################################################################
+	# PLEASE FJERN DET HER SENERE DET ER UDELUKKENDE FOR TEST AF BAGGRUNDE
+	if StateMachine._dayCount / 20.0 == frameIndex+1:
+		frameIndex += 1
+		get_node("../../Background_Stue").frame = frameIndex
+	#######################################################################
 
--- scripts/StateMachine.gd
+++ scripts/StateMachine.gd
@@ -10,58 +10,58 @@ var _dayTimer: Timer
 var _dayCount: int = 0
 
 var upgradeInfo: Dictionary = {
-    "range": { # magical game units (aka. mayhaps pixels)
-        "base": 40.,
-        "bigger_colonies": 10.
-    }, # colonyRange
-    "max_size": { # max number of cells in colony
-        "base": 1_000_000.,
-        "bigger_colonies": 1_500_000., # duplicate upgrade name means it changes multiple stats
-    }, # max size
-    "growth_multiplier": {
-        "base": 1.,
-        "bigger_colonies": -.1,
-        "giga_growth": .2,
-    }, # growth multiplier
-    "spread_amount": { # Amount of cells colonies spread to adjesant colonies pr. day
-        "base": 5000.,
-    }, # spread amount
-    "growth_decrease": { # cells pr. day
-        "base": 1000.,
-    }, # growth decrease
-    "damage_multiplyer": {
-        "base": 1.,
-    }, # damage multiplier
-    "misc.": {
-        "organ_spread": 0
-    }, # misc
+	"range": { # magical game units (aka. mayhaps pixels)
+		"base": 40.,
+		"bigger_colonies": 10.
+	}, # colonyRange
+	"max_size": { # max number of cells in colony
+		"base": 1_000_000.,
+		"bigger_colonies": 1_500_000., # duplicate upgrade name means it changes multiple stats
+	}, # max size
+	"growth_multiplier": {
+		"base": 1.,
+		"bigger_colonies": -.1,
+		"giga_growth": .2,
+	}, # growth multiplier
+	"spread_amount": { # Amount of cells colonies spread to adjesant colonies pr. day
+		"base": 5000.,
+	}, # spread amount
+	"growth_decrease": { # cells pr. day
+		"base": 1000.,
+	}, # growth decrease
+	"damage_multiplyer": {
+		"base": 1.,
+	}, # damage multiplier
+	"misc.": {
+		"organ_spread": 0
+	}, # misc
 }
 
 var upgradeUIInfo: Dictionary = {
-    "Default": {
-        "Description": "Unlock your first cancer colony.",
-        "State": "Unlocked", # Three states: Locked, unlocked and bought.
-        "Cost": 0.,
-        "Active": false
-    },
-    "Giga Growth": {
-        "Description": "Increase the growth-rate of your cancer by xxx.",
-        "State": "Locked",
-        "Cost": 10.,
-        "Active": false
-    },
-    "Lymph Nodes": {
-        "Description": "Gain access to the lymph nodes.",
-        "State": "Locked",
-        "Cost": 10.,
-        "Active": false
-    },
-    "Medical Resistance": {
-        "Description": "Become more resistant to cancer treatment.",
-        "State": "Locked",
-        "Cost": 10.,
-        "Active": false
-    }
+	"Default": {
+		"Description": "Unlock your first cancer colony.",
+		"State": "Unlocked", # Three states: Locked, unlocked and bought.
+		"Cost": 0.,
+		"Active": false
+	},
+	"Giga Growth": {
+		"Description": "Increase the growth-rate of your cancer by xxx.",
+		"State": "Locked",
+		"Cost": 10.,
+		"Active": false
+	},
+	"Lymph Nodes": {
+		"Description": "Gain access to the lymph nodes.",
+		"State": "Locked",
+		"Cost": 10.,
+		"Active": false
+	},
+	"Medical Resistance": {
+		"Description": "Become more resistant to cancer treatment.",
+		"State": "Locked",
+		"Cost": 10.,
+		"Active": false
+	}
 }
 
 
@@ -74,55 +74,55 @@ signal OnNextDay
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    # Start the day timer
-    _dayTimer = Timer.new()
-    _dayTimer.wait_time = 1
-    add_child(_dayTimer)
-    _dayTimer.start()
-    _dayTimer.timeout.connect(_IncementDayCounter)
+	# Start the day timer
+	_dayTimer = Timer.new()
+	_dayTimer.wait_time = 1
+	add_child(_dayTimer)
+	_dayTimer.start()
+	_dayTimer.timeout.connect(_IncementDayCounter)
 
 
 func _IncementDayCounter():
-    _dayCount += 1
-    print("Day: ", _dayCount)
-    OnNextDay.emit()
+	_dayCount += 1
+	print("Day: ", _dayCount)
+	OnNextDay.emit()
 
 
 func ChangeDaySpeed(speed: float):
-    # This updates the timers speed immediatly
-    var timeRatio = _dayTimer.wait_time / (1 / speed)
-    _dayTimer.wait_time = _dayTimer.time_left / timeRatio  # This scales the remaining time by ratio
-    _dayTimer.stop()
-    _dayTimer.start()  # restart the timer with the new remaining time
-    _dayTimer.wait_time = 1 / speed  # set the timer to it's actual speed
+	# This updates the timers speed immediatly
+	var timeRatio = _dayTimer.wait_time / (1 / speed)
+	_dayTimer.wait_time = _dayTimer.time_left / timeRatio  # This scales the remaining time by ratio
+	_dayTimer.stop()
+	_dayTimer.start()  # restart the timer with the new remaining time
+	_dayTimer.wait_time = 1 / speed  # set the timer to it's actual speed
 
 
 func GetCurrentDay() -> int:
-    return _dayCount
+	return _dayCount
 
 
 func GetTotalNumCells() -> int:
-    var numCells := 0
-    for colony in colonies:
-        numCells += colony.colonySize
+	var numCells := 0
+	for colony in colonies:
+		numCells += colony.colonySize
 
-    return numCells
+	return numCells
 
 
 func GetActiveColonies() -> Array[Colony]:
-    var activeColonies: Array[Colony] = []
-    for colony in colonies:
-        if colony.active:
-            activeColonies.append(colony)
+	var activeColonies: Array[Colony] = []
+	for colony in colonies:
+		if colony.active:
+			activeColonies.append(colony)
 
-    return activeColonies
+	return activeColonies
 
 
 func StopDayCycle():
-    _dayTimer.stop()
+	_dayTimer.stop()
 
 
 func ResumeDayCycle():
-    _dayTimer.start()
+	_dayTimer.start()
 
 
